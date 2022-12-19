15dce301ed0f1999bbc6d0d33ee71c8773821685
fhero2030 <jcarloasilo@gmail.com>
2022-12-19 14:55:23 +0800

    Added meta img

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import JcarloMetaImg from "./assets/jcarlo-meta-img.png";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <meta property="og:image" itemProp="image" content={JcarloMetaImg} />
     <App />
   </React.StrictMode>
-)
+);
