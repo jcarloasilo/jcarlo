21b2e2d99ec99035de8bb6fee241fbd1d2a0d2d3
fhero2030 <jcarloasilo@gmail.com>
2023-06-07 20:34:28 +0800

    add hataw takbo

---
+++ src/assets/portfolio/hataw-takbo/hataw-takbo.ts
@@ -0,0 +1,8 @@
+import Image1 from "./00-hataw-takbo.webp";
+import Image2 from "./01-hataw-takbo.webp";
+import Image3 from "./02-hataw-takbo.webp";
+import Image4 from "./03-hataw-takbo.webp";
+import Image5 from "./04-hataw-takbo.webp";
+import Image6 from "./05-hataw-takbo.webp";
+
+export default [Image1, Image2, Image3, Image4, Image5, Image6];
