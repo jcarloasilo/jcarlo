acd6daeb454d6a2c4359b79dcdb1fae6a0133a92
fhero2030 <jcarloasilo@gmail.com>
2023-07-28 18:24:30 +0800

    initial revamp commit

--- src/assets/images.ts
+++
@@ -1,5 +0,0 @@
-export { default as BosscoImages } from "./portfolio/1bossco/1bossco";
-export { default as IctInventoryImages } from "./portfolio/ict-inventory/ict-inventory";
-export { default as CibmImages } from "./portfolio/cibm/cibm";
-export { default as BloodDonationImages } from "./portfolio/blood-donation/blood-donation";
-export { default as HatawTakboImages } from "./portfolio/hataw-takbo/hataw-takbo";
